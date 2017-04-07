46998bc0ee6cd6cc8b75a952e6ec91035ff4d163
Ved Petkar <vedpetkar@gmail.com>
2017-04-06 22:56:45 -0400

    Add student as default role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   belongs_to :role
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  private
+
+  def set_default_role
+    self.role ||= Role.find_by_name('student')
+  end
 end

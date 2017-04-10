fbf22d8fa34a400b3944c8d7a8c784616d2f882b
Ved Petkar <vedpetkar@gmail.com>
2017-04-10 15:47:29 -0400

    Track progress for sharing links

--- app/assets/javascripts/components/share_progress.es6.jsx
+++ app/assets/javascripts/components/share_progress.es6.jsx
@@ -4,32 +4,36 @@ class ShareProgress extends React.Component {
 
     this.state = {
       percent: 0,
-      color: '#3FC7FA'
+      color: '#3FC7FA',
+      facebookShared: false,
+      twitterShared: false,
     }
     this.shareToFacebook = this.shareToFacebook.bind(this);
-    this.changeState = this.changeState.bind(this);
+    this.shareToTwitter = this.shareToTwitter.bind(this);
   }
   shareToFacebook() {
-    this.changeState();
+    if (!this.state.facebookShared) {
+      this.setState({ facebookShared: true, percent: this.state.percent + 50 });
+    }
     window.open("https://www.facebook.com/dialog/feed?app_id=184683071273&link=https%3A%2F%2Ffacebook.com%2Fgetpique&picture=http%3A%2F%2Fwww.insert-image-share-url-here.jpg&name=This%20is%20a%20Sample%20Title&caption=%20&description=This%20is%20a%20Facebook%20description.&redirect_uri=http%3A%2F%2Fwww.facebook.com%2F");
   }
-  changeState() {
-    console.log("yolo")
-    if(this.state.percent < 99) {
-      this.state.percent = this.state.percent + 33;
-    } else {
-      this.state.percent = 99.99;
+  shareToTwitter() {
+    if(!this.state.twitterShared) {
+      this.setState({ twitterShared: true, percent: this.state.percent + 50 });
     }
-    this.setState({
-      percent: this.state.percent
-    });
+    window.open("http://twitter.com/intent/tweet?text=Get%20pique!%20https%3A%2F%2Fgetpique.co");
   }
   render () {
     return (
       <div>
-          <button onClick={this.shareToFacebook}> Share on Facebook </button>
-          <a href="https://twitter.com/getpique">Share on Twitter</a>
-        <ProgressLine onClick={this.changeState} percent={this.state.percent} strokeWidth="4" strokeColor={this.state.color}>
+        <div>
+          <button className="facebook" onClick={this.shareToFacebook}> Share on Facebook </button>
+        </div>
+        <div>
+          <button className="twitter" onClick={this.shareToTwitter}> Share on Twitter </button>
+        </div>
+        <p> Progress: {this.state.percent}% </p>
+        <ProgressLine percent={this.state.percent} strokeWidth="4" strokeColor={this.state.color}>
         </ProgressLine>
       </div>
     );

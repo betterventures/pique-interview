22f181676f5e546242b1e0ae12e1d620b0524aa7
Ved Petkar <vedpetkar@gmail.com>
2017-04-10 15:16:35 -0400

    Share to Facebook button increases progress

--- app/assets/javascripts/components/share_progress.es6.jsx
+++ app/assets/javascripts/components/share_progress.es6.jsx
@@ -6,10 +6,15 @@ class ShareProgress extends React.Component {
       percent: 0,
       color: '#3FC7FA'
     }
-
+    this.shareToFacebook = this.shareToFacebook.bind(this);
     this.changeState = this.changeState.bind(this);
   }
+  shareToFacebook() {
+    this.changeState();
+    window.open("https://www.facebook.com/dialog/feed?app_id=184683071273&link=https%3A%2F%2Ffacebook.com%2Fgetpique&picture=http%3A%2F%2Fwww.insert-image-share-url-here.jpg&name=This%20is%20a%20Sample%20Title&caption=%20&description=This%20is%20a%20Facebook%20description.&redirect_uri=http%3A%2F%2Fwww.facebook.com%2F");
+  }
   changeState() {
+    console.log("yolo")
     if(this.state.percent < 99) {
       this.state.percent = this.state.percent + 33;
     } else {
@@ -21,8 +26,12 @@ class ShareProgress extends React.Component {
   }
   render () {
     return (
-      <ProgressLine onClick={this.changeState} percent={this.state.percent} strokeWidth="4" strokeColor={this.state.color}>
-      </ProgressLine>
+      <div>
+          <button onClick={this.shareToFacebook}> Share on Facebook </button>
+          <a href="https://twitter.com/getpique">Share on Twitter</a>
+        <ProgressLine onClick={this.changeState} percent={this.state.percent} strokeWidth="4" strokeColor={this.state.color}>
+        </ProgressLine>
+      </div>
     );
   }
 }

7e62122c71cfc3b4d549c89ad33c954b0cc6b33f
Ved Petkar <vedpetkar@gmail.com>
2017-04-27 20:24:48 -0400

    Add association to scholarship

--- app/models/scholarship.rb
+++ app/models/scholarship.rb
@@ -1,2 +1,5 @@
 class Scholarship < ApplicationRecord
+  has_many :locations, inverse_of: :scholarship
+  accepts_nested_attributes_for :locations, reject_if: :all_blank,
+                                            allow_destroy: true
 end

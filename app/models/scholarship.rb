d058752098a806209b4f792cdbc42623457a201f
Ved Petkar <vedpetkar@gmail.com>
2017-04-28 20:21:37 -0400

    Accept nested attributes for supplemental requirements

--- app/models/scholarship.rb
+++ app/models/scholarship.rb
@@ -1,5 +1,8 @@
 class Scholarship < ApplicationRecord
   has_many :location_limitations, inverse_of: :scholarship
   accepts_nested_attributes_for :location_limitations, reject_if: :all_blank,
-                                            allow_destroy: true
+                                                       allow_destroy: true
+  accepts_nested_attributes_for :supplemental_requirements,
+                                reject_if: :all_blank,
+                                allow_destroy: true
 end

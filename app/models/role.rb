a626e15cb9a4652567f65adaebe292a29606a6ad
Frank Kotsianas <frankkotsianas@gmail.com>
2017-05-10 17:45:48 -0700

    Define role types in Role model
    - Role table > User table attribute because we have scenarios where a
    User can have multiple roles (eg, Provider + Reviewer on another
    application; or Provider + Recommender)

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,23 @@
 class Role < ApplicationRecord
   has_many :users, dependent: :destroy
 
-  PROVIDER_TYPE = 1
+  PROVIDER_ROLE = 'provider'
+  RECOMMENDER_ROLE = 'recommender'
+  REVIEWER_ROLE = 'reviewer'
+  STUDENT_ROLE = 'student'
+
+  ROLE_TYPES = [
+    PROVIDER_ROLE,
+    RECOMMENDER_ROLE,
+    REVIEWER_ROLE,
+    STUDENT_ROLE
+  ]
+
+  # Role type helpers
+  # fetch the desired Role record - e.g. `Role.provider`
+  scope :provider, -> { where(name: PROVIDER_ROLE).first }
+  scope :recommender, -> { where(name: RECOMMENDER_ROLE).first }
+  scope :reviewer, -> { where(name: REVIEWER_ROLE).first }
+  scope :student, -> { where(name: STUDENT_ROLE).first }
+
 end

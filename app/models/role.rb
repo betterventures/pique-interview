c11fa6df7a4881f859c1f53e731feb8b175f8135
Frank Kotsianas <frankkotsianas@gmail.com>
2017-05-10 17:16:08 -0700

    User can belong to an Organization
    - removes need for Provider model - prefer STI (or in our case, roles)
    - PROVIDER_TYPE is 1

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,5 @@
 class Role < ApplicationRecord
   has_many :users, dependent: :destroy
+
+  PROVIDER_TYPE = 1
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430160622) do
+ActiveRecord::Schema.define(version: 20170511001200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -125,7 +125,9 @@ ActiveRecord::Schema.define(version: 20170430160622) do
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
     t.integer  "role_id"
+    t.integer  "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["organization_id"], name: "index_users_on_organization_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["role_id"], name: "index_users_on_role_id", using: :btree
   end
@@ -133,5 +135,6 @@ ActiveRecord::Schema.define(version: 20170430160622) do
   add_foreign_key "location_limitations", "scholarships"
   add_foreign_key "providers", "organizations"
   add_foreign_key "supplemental_requirements", "scholarships"
+  add_foreign_key "users", "organizations"
   add_foreign_key "users", "roles"
 end

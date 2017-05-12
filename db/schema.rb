9caa36d982983f68b65ed7bc67c9910343fb4dfe
Frank Kotsianas <frankkotsianas@gmail.com>
2017-05-12 02:44:57 -0700

    Add `address` to Organizations
    - street address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170511101636) do
+ActiveRecord::Schema.define(version: 20170512094403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170511101636) do
     t.string   "website"
     t.string   "city"
     t.string   "state"
+    t.string   "address"
   end
 
   create_table "payments", force: :cascade do |t|

d5dcadaa0cf311bbfcdb390e3dffe2c1e352c0a6
Frank Kotsianas <frankkotsianas@gmail.com>
2017-05-16 06:58:43 -0700

    Auth error messages appear in auth modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  # return devise errors, one at a time, for display in auth views
+  def devise_error_flash
+    return "" unless devise_error_messages?
+
+    resource.errors.full_messages.last.downcase.capitalize + "."
+  end
+
+  def alert
+    super && super.downcase.capitalize
+  end
+
+  def devise_error_messages?
+    !resource.errors.empty?
+  end
 end

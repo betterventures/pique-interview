0013207b69584096d1dc42832dbe80ae2f891b93
Frank Kotsianas <frankkotsianas@gmail.com>
2017-05-24 21:21:57 -0700

    Scholarship has_many :applicants and Student has_many :scholarship_applications

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,9 @@
 # - eventually will belong_to a :school
 class Student < User
 
+  has_many :scholarship_applications, inverse_of: :user, dependent: :destroy
+  has_many :applied_scholarships, through: :scholarship_applications, source: :scholarship
+
   private
 
   def set_default_role

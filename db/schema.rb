200aafc0fadac474855b3c75b211beb5cc21d76e
Frank Kotsianas <frankkotsianas@gmail.com>
2017-05-24 17:52:36 -0700

    Drop Providers table in favor of STI on Users table
    - enum for user types
    - 3 types with clear division: Providers, Students, and Educators

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523032857) do
+ActiveRecord::Schema.define(version: 20170525005106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,25 +69,6 @@ ActiveRecord::Schema.define(version: 20170523032857) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.index ["organization_id"], name: "index_payments_on_organization_id", using: :btree
-  end
-
-  create_table "providers", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "organization_id"
-    t.index ["email"], name: "index_providers_on_email", unique: true, using: :btree
-    t.index ["organization_id"], name: "index_providers_on_organization_id", using: :btree
-    t.index ["reset_password_token"], name: "index_providers_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "scholarships", force: :cascade do |t|
@@ -166,7 +147,6 @@ ActiveRecord::Schema.define(version: 20170523032857) do
   add_foreign_key "essay_prompts", "essay_requirements"
   add_foreign_key "essay_requirements", "scholarships"
   add_foreign_key "location_limitations", "scholarships"
-  add_foreign_key "providers", "organizations"
   add_foreign_key "supplemental_requirements", "scholarships"
   add_foreign_key "users", "organizations"
 end

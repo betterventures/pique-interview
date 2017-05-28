b02c366547a0d87e64f7500c7db7d7d3c3c98580
Frank Kotsianas <frankkotsianas@gmail.com>
2017-05-28 03:59:01 -0700

    Delete old Webpacker config files
    - bring back when upgrade to r_o_r 8

--- config/webpack/loaders/babel.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  test: /\.js(\.erb)?$/,
-  exclude: /node_modules/,
-  loader: 'babel-loader',
-  options: {
-    presets: ['env', 'react', 'es2016', 'es2015', 'stage-0', 'stage-2']
-  }
-}

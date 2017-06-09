b81485efb7dbb59be72bd80fd59e8e69b03a7626
Frank Kotsianas <frankkotsianas@gmail.com>
2017-06-09 20:45:36 +0300

    Ellipsis description text on dashboard cards

--- client/app/containers/Protected/Provider/Dashboard/DashboardCards/index.js
+++ client/app/containers/Protected/Provider/Dashboard/DashboardCards/index.js
@@ -13,7 +13,18 @@ const DashboardCards = ({ items }) => {
               <div className={css.img} style={{backgroundImage: `url(${x.image})`}} />
               <div className={css.stripe}>
                 <div className={css.name}>{x.name}</div>
-                <div className={css.desc}>{x.description}</div>
+                <div className={css.desc}>
+                  {
+                    x.description && x.description.length > 105
+                      ?
+                        // Hacky multiline ellipsis: add ellipsis after n chars,
+                        // and if n+1-th char is a \s, then add a space before the ellipsis.
+                        // This preserves appearance when last char is \s as well as \w.
+                        `${x.description.substr(0,105)}${x.description[105] === ' ' ? ' ' : ''}...`
+                      :
+                        x.description
+                  }
+                </div>
                 <div
                   className={css.rating}
                   style={{height: '28px', 'fontWeight': 400}}

2f5e6185b266f6f858677768106b5f9109557c62
Frank Kotsianas <frankkotsianas@gmail.com>
2017-06-09 12:58:37 +0300

    EssayReq upload boxes on ScholPreview are dynamic to # of essays

--- client/app/containers/Protected/Common/IndividualScholarships/IndividualScholarshipsFooter/Apply/index.js
+++ client/app/containers/Protected/Common/IndividualScholarships/IndividualScholarshipsFooter/Apply/index.js
@@ -13,27 +13,37 @@ export class IndividualScholarshipsApply extends Component {
       while (++i <= scholarship.minimum_recommendations) recommendations.push(i)
     }
 
+    let essayRequirements = []
+    if (scholarship.essay_requirements.length > 0) {
+      let i = 0
+      while (++i <= scholarship.essay_requirements.length) essayRequirements.push(i)
+    }
+
+
     return (
       <div className={css.root}>
         <div className={css.copy}>{`Select the items from your Pique Proflie you would like to use to apply to the ${scholarship.title} Scholarship. Your ‘cover letter’ and ‘official transcript’ will automatically be sent with your application.`}</div>
         {
-          recommendations.map((i) => (
+          recommendations.map(i => (
             <div className={css.row} key={i}>
               <div className={css.label}>Recommendation #{i}</div>
               <div className={css.field}>
                 <select className={css.input}type="text"/>
               </div>
             </div>
-            )
-          )
+          ))
         }
-        <div className={css.row}>
-          <div className={css.label}>Scholarship Essay</div>
-          <div className={css.field}>
-            <select className={css.input}type="text"/>
-          </div>
+        {
+          essayRequirements.map(i => (
+            <div className={css.row} key={i}>
+              <div className={css.label}>Scholarship Essay #{i}</div>
+              <div className={css.field}>
+                <select className={css.input}type="text"/>
+              </div>
+            </div>
+          ))
+        }
         </div>
-      </div>
     )
   }
 }

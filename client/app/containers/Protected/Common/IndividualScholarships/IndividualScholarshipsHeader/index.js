a509b775f16fe727e6496335387e1e66a434a92b
Frank Kotsianas <frankkotsianas@gmail.com>
2017-07-17 18:45:17 -0700

    Clamp 'Read More' text to 4 lines on Schol Preview
    - entire <p> is clickable to toggle

--- client/app/containers/Protected/Common/IndividualScholarships/IndividualScholarshipsHeader/index.js
+++ client/app/containers/Protected/Common/IndividualScholarships/IndividualScholarshipsHeader/index.js
@@ -2,26 +2,73 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { ButtonFill } from '../IndividualScholarshipsBtns'
 import * as Actions from 'api/actions'
+import clamp from 'line-clamp'
 import css from './style.css'
 
-const SHORT_HEADER_LENGTH = 60
+const CLAMPABLE_HEADER_ID = 'headerToClamp'
 
 export class IndividualScholarshipsHeader extends Component {
 
   constructor(props) {
     super(props)
-    this.toggleHeaderExpansion = ::this.toggleHeaderExpansion
+    this.toggleHeaderClamp = ::this.toggleHeaderClamp
+    this.clampHeaderInDOM = ::this.clampHeaderInDOM
 
     this.state = {
-      headerExpanded: this.props.scholarship.description &&
-                    this.props.scholarship.description.length <= SHORT_HEADER_LENGTH
+      shouldClampHeader: true
     }
   }
 
-  toggleHeaderExpansion() {
+  toggleHeaderClamp() {
     this.setState({
-      headerExpanded: !this.state.headerExpanded
+      shouldClampHeader: !this.state.shouldClampHeader
     })
+  }
+
+  readMoreLessHTML() {
+    return `<a style="color: #599CD9; cursor: pointer;">Read ${this.state.shouldClampHeader ? 'More' : 'Less'}</a>`
+  }
+
+  /* Note on DIRECTLY CLAMPING THE DOM:
+   * This is a little wonky because we set state here,
+   * then expect an update directly to the DOM,
+   * that is not reflected in React.
+   *
+   * We deal with this by expecting state: shouldClampHeader
+   * to correlate exactly with the DOM state.
+   *
+   * Ideally, however, we would truncate ('clamp') the text in
+   * the React, then preferentially render that based on state.
+   *
+   * Because this requires knowing line length dynamically
+   * (see: [https://github.com/yuanqing/line-clamp/blob/master/src/index.js](line-clamp)),
+   * we skip this for now.
+   *
+   * The key issue is that it is difficult to know the length
+   * of the line in characters, which would allow us to truncate it.
+   */
+  clampHeaderInDOM(linesToClamp=4) {
+    let textToClamp = document.getElementById(CLAMPABLE_HEADER_ID)
+    clamp(textToClamp, {
+      lineCount: linesToClamp,
+      ellipsisCharacter: `... ${this.readMoreLessHTML()}`
+    })
+  }
+
+  componentDidMount() {
+    /* Default to clamped state */
+    this.clampHeaderInDOM()
+  }
+
+  componentDidUpdate() {
+    /* Clamp header in DOM if should be clamped */
+    this.state.shouldClampHeader
+      ?
+        /* Clamp text */
+        this.clampHeaderInDOM(4)
+      :
+        /* Clamp text to the maximum */
+        ''
   }
 
   render() {
@@ -31,28 +78,23 @@ export class IndividualScholarshipsHeader extends Component {
         <div className={css.header}>{scholarship.title}</div>
         <div className={css.copy}>
           {
-            this.state.headerExpanded
+            /* Slightly unnecessary but we need this to rerender
+             * in order to remove clamping.
+             */
+            this.state.shouldClampHeader
               ?
-                scholarship.description
+                <p id={CLAMPABLE_HEADER_ID}
+                  onClick={() => this.toggleHeaderClamp()}
+                  className={css.clampabletext}>
+                   { scholarship.description }
+                </p>
               :
-                scholarship.description.substr(0, SHORT_HEADER_LENGTH)
-          }
-          {
-            this.state.headerExpanded
-              ?
-                <span
-                  className={css.less}
-                  onClick={this.toggleHeaderExpansion}
-                >
-                  Read Less
-                </span>
-              :
-                <span
-                  className={css.more}
-                  onClick={this.toggleHeaderExpansion}
-                >
-                  Read More
-                </span>
+                <div>
+                  <p onClick={() => this.toggleHeaderClamp()}
+                    className={css.clampabletext}>
+                    { scholarship.description }
+                  </p>
+                </div>
           }
         </div>
 

a7c6f3d95eedf8fc8d82239f985d3e435cdc4956
Frank Kotsianas <frankkotsianas@gmail.com>
2017-07-20 15:18:27 -0700

    Allow Student to update, not just create, ParentOrGuardians
    - really we should just create them separately but for the purposes of
    dummy data this is helpful

--- app/models/student.rb
+++ app/models/student.rb
@@ -23,11 +23,7 @@ class Student < User
                                 },
                                 allow_destroy: true
   accepts_nested_attributes_for :parent_or_guardian_relationships,
-                                reject_if: ->(attrs) {
-                                  !UserToStudentRelationship.relationship_types.keys.include?(
-                                    attrs['relationship_type'].to_s
-                                  )
-                                },
+                                reject_if: :validate_and_fire_parent_or_guardian_update_hook!,
                                 allow_destroy: true
 
   def gpa_string
@@ -61,5 +57,35 @@ class Student < User
 
   def set_default_role
     self.role ||= self.class.roles[:student]
+  end
+
+  # really ugly was of allowing us to update parent relations
+  # The problem: creating works fine but updating fails with taken email -
+  # I can't seem to find a way to intercept parent_or_guardian_attributes= on
+  # the UserToStudentRelationship relation.
+  # So, check if the parent exists here using the provided hook -
+  # only instead of rejecting, we use the hook to fire off an alternative update.
+  #
+  # In the future we probably shouldn't be updating data this way (through the student)
+  # (or perhaps we should just separately create the parents first and then pass a find_by/id to the dummy data),
+  # but for now w/e. We want to set up dummy data without knowing if the student exists yet.
+  def validate_and_fire_parent_or_guardian_update_hook!(attrs)
+    !UserToStudentRelationship.relationship_types.keys.include?(
+      attrs['relationship_type'].to_s
+    ) ||
+    update_parent_or_guardian_if_exists?(attrs)
+  end
+
+  def update_parent_or_guardian_if_exists?(attrs)
+    desired_parent_or_guardian = ParentOrGuardian.find_by(
+      email: attrs['parent_or_guardian_attributes']['email'].downcase
+    )
+    # update or set guardian
+    if self.parent_or_guardians.include?(desired_parent_or_guardian)
+      desired_parent_or_guardian.update_attributes!(attrs['parent_or_guardian_attributes'])
+    else
+      new_parent_or_guardian = self.parent_or_guardians.build(attrs['parent_or_guardian_attributes'])
+      new_parent_or_guardian.save!
+    end
   end
 end

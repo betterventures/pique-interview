6feec7caefa23dd5aa874822e7cda400b62d484b
Frank Kotsianas <frankkotsianas@gmail.com>
2017-07-19 17:43:32 -0700

    Add validations to Score Card Create 'Add Criterion' fields

--- client/app/containers/Protected/Provider/ScholarshipScoreCard/index.js
+++ client/app/containers/Protected/Provider/ScholarshipScoreCard/index.js
@@ -10,6 +10,7 @@ export class ScholarshipScoreCard extends Component {
     this.state = {
       title: '',
       possible_score: '',
+      invalidAttrs: [],
     }
     this.setNextFieldTitle = ::this.setNextFieldTitle
     this.setNextFieldScore = ::this.setNextFieldScore
@@ -29,6 +30,33 @@ export class ScholarshipScoreCard extends Component {
   }
 
   addScoreCardField() {
+    // Validate inputs and turn on validation states
+    let failedValidation = []
+    if (
+      this.state.title.length < 1
+    ) {
+      failedValidation.push('a Criterion Name')
+    }
+    if (
+      this.state.possible_score.length < 1 ||
+      isNaN(Number(this.state.possible_score))
+    ) {
+      failedValidation.push('a number for Possible Score')
+    }
+
+    console.log("VALIDATING")
+    console.log(this.state.invalidAttrs)
+
+    this.setState({
+      invalidAttrs: failedValidation
+    })
+    if (
+      failedValidation.length > 0
+    ) {
+      return
+    }
+
+    // Validation passed
     // Call into reducer - Reducer modifies global state, triggers rerender
     this.props.addScoreCardField({
       title: this.state.title,
@@ -50,6 +78,17 @@ export class ScholarshipScoreCard extends Component {
             <div className={css.comment}>
               A Scholarship Score Card allows you to create criteria by which your selection committee can evaluate all scholarship applicants.
             </div>
+            {
+              this.state.invalidAttrs && this.state.invalidAttrs.length > 0
+                ?
+                  <div className={css.error}>
+                    {
+                      `Please provide ${ this.state.invalidAttrs.join(' and ') }!`
+                    }
+                  </div>
+                :
+                  ''
+            }
             <div className={css.borderrow}>
               <input className={css.lginput} type="text" placeholder="Add Criteria" onChange={this.setNextFieldTitle} value={this.state.title} />
               <input className={css.sminput} type="text" placeholder="Total Possible Score" onChange={this.setNextFieldScore} value={this.state.possible_score} />

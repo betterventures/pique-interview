75b9d6474d9dfe70d8b883243593b469bfe4e2b0
Frank Kotsianas <frankkotsianas@gmail.com>
2017-07-22 17:59:33 -0700

    Destroy associated App Rating Fields when Score Card Field is destroyed

--- app/models/application_rating_field.rb
+++ app/models/application_rating_field.rb
@@ -1,4 +1,4 @@
 class ApplicationRatingField < ApplicationRecord
   belongs_to :application_rating, inverse_of: :fields
-  belongs_to :score_card_field
+  belongs_to :score_card_field, inverse_of: :application_rating_fields
 end
--- app/models/score_card_field.rb
+++ app/models/score_card_field.rb
@@ -3,4 +3,6 @@ class ScoreCardField < ApplicationRecord
 
   validates :title, presence: true
   validates :possible_score, presence: true
+
+  has_many :application_rating_fields, inverse_of: :score_card_field, dependent: :destroy
 end

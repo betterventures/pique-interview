96e9afe26a7c1149f006e4177886c65719351cbd
Frank Kotsianas <frankkotsianas@gmail.com>
2017-07-31 00:05:22 -0700

    Add Show More buttons to Applicant Activities and Awards

--- client/app/containers/Protected/Common/Applicant/ApplicantUploads/About/index.js
+++ client/app/containers/Protected/Common/Applicant/ApplicantUploads/About/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Page from 'components/Icons/Page'
 import DocumentStarLines from 'components/Icons/DocumentStarLines'
@@ -12,66 +12,124 @@ function formatDate(awardDate) {
   return moment(awardDate).format(AWARD_DATE_FORMAT)
 }
 
-export const ApplicantAbout = ({ intro, description, activities=[], honors_and_awards=[] }) => {
-  let applicantActivities = activities.map(act => JSON.parse(act))
-  let applicantHonorsAndAwards = honors_and_awards.map(honor => JSON.parse(honor))
+export class ApplicantAbout extends Component {
+  constructor(props) {
+    super(props)
+    this.toggleActivitiesClosed = ::this.toggleActivitiesClosed
+    this.toggleAwardsClosed = ::this.toggleAwardsClosed
 
-  return (
-    <div className={css.root}>
-      <div className={css.header}>About</div>
-      <div className={css.box}>
-        <div className={css.section}>
-          <div className={css.title}>
-            <Page className={css.icon} />
-            <span className={css.text}>Intro</span>
-          </div>
-          {intro
-            ? <div className={css.copy}>{intro}</div>
-            : <div className={css.placeholder}>{introPlaceholder}</div>}
-        </div>
+    this.state = {
+      activitiesClosed: true,
+      honorsAndAwardsClosed: true,
+    }
+  }
 
-        <div className={css.section}>
-          <div className={css.title}>
-            <DocumentStarLines className={css.icon} />
-            <span className={css.text}>Activities</span>
-          </div>
-          {applicantActivities.map((x, i) =>
-            <div className={css.activity} key={i}>
-              <div className={css.position}>
-                <div className={css.strong}>{x.position_held}</div>
-                <div className={css.strong}>{ formatDate(x.start_date) } - { formatDate(x.end_date) }</div>
-              </div>
-              <div className={css.area}>{x.title}</div>
-              <div className={css.copy}>{x.description}</div>
+  toggleActivitiesClosed() {
+    this.setState({
+      activitiesClosed: !this.state.activitiesClosed
+    })
+  }
+
+  toggleAwardsClosed() {
+    this.setState({
+      honorsAndAwardsClosed: !this.state.honorsAndAwardsClosed
+    })
+  }
+
+  render() {
+    const { intro, description, activities=[], honors_and_awards=[] } = this.props
+    let applicantActivities = activities.map(act => JSON.parse(act))
+    let applicantHonorsAndAwards = honors_and_awards.map(honor => JSON.parse(honor))
+
+    if (this.state.activitiesClosed) {
+      applicantActivities = applicantActivities.slice(0,3)
+    }
+
+    if (this.state.honorsAndAwardsClosed) {
+      applicantHonorsAndAwards = applicantHonorsAndAwards.slice(0,3)
+    }
+
+    return (
+      <div className={css.root}>
+        <div className={css.header}>About</div>
+        <div className={css.box}>
+          <div className={css.section}>
+            <div className={css.title}>
+              <Page className={css.icon} />
+              <span className={css.text}>Intro</span>
             </div>
-          )}
-          {applicantActivities.length
-            ? <div className={css.placeholder}>Add Another Activity</div>
-            : <div className={css.placeholder}>{activitiesPlaceholder}</div>}
+            {intro
+              ? <div className={css.copy}>{intro}</div>
+              : <div className={css.placeholder}>{introPlaceholder}</div>}
+          </div>
+
+          <div className={css.section}>
+            <div className={css.title}>
+              <DocumentStarLines className={css.icon} />
+              <span className={css.text}>Activities</span>
+            </div>
+            {applicantActivities.map((x, i) =>
+              <div className={css.activity} key={i}>
+                <div className={css.position}>
+                  <div className={css.strong}>{x.position_held}</div>
+                  <div className={css.strong}>{ formatDate(x.start_date) } - { formatDate(x.end_date) }</div>
+                </div>
+                <div className={css.area}>{x.title}</div>
+                <div className={css.copy}>{x.description}</div>
+              </div>
+            )}
+            {
+              activities.length > 3
+                ?
+                  <a
+                    className={css.link}
+                    onClick={this.toggleActivitiesClosed}
+                  >
+                    <i>{ this.state.activitiesClosed ? 'Show More' : 'Show Less' }</i>
+                  </a>
+                :
+                  ''
+            }
+            {applicantActivities.length
+              ? ''
+              : <div className={css.placeholder}>{activitiesPlaceholder}</div>}
+          </div>
+
+          <div className={css.section}>
+            <div className={css.title}>
+              <DocumentStarAward className={css.icon} />
+              <span className={css.text}>Honors & Awards</span>
+            </div>
+            {applicantHonorsAndAwards.map((x, i) =>
+              <div className={css.activity} key={i}>
+                <div className={css.position}>
+                  <div className={css.strong}>{x.title}</div>
+                  <div className={css.strong}>{ formatDate(x.awarded_at) }</div>
+                </div>
+                <div className={css.area}>{x.provider_name}</div>
+              </div>
+            )}
+            {
+              honors_and_awards.length > 3
+                ?
+                  <a
+                    className={css.link}
+                    onClick={this.toggleAwardsClosed}
+                  >
+                    <i>{ this.state.honorsAndAwardsClosed ? 'Show More' : 'Show Less' }</i>
+                  </a>
+                :
+                  ''
+            }
+            {applicantHonorsAndAwards.length
+              ? ''
+              : <div className={css.placeholder}>{honorsAndAwardsPlaceholder}</div>}
+          </div>
         </div>
 
-        <div className={css.section}>
-          <div className={css.title}>
-            <DocumentStarAward className={css.icon} />
-            <span className={css.text}>Honors & Awards</span>
-          </div>
-          {applicantHonorsAndAwards.map((x, i) =>
-            <div className={css.activity} key={i}>
-              <div className={css.position}>
-                <div className={css.strong}>{x.title}</div>
-                <div className={css.strong}>{ formatDate(x.awarded_at) }</div>
-              </div>
-              <div className={css.area}>{x.provider_name}</div>
-            </div>
-          )}
-          {applicantHonorsAndAwards.length
-            ? <div className={css.placeholder}>Add Another Honor or Award</div>
-            : <div className={css.placeholder}>{honorsAndAwardsPlaceholder}</div>}
-        </div>
       </div>
-
-    </div>
-  )
+    )
+  }
 }
 
 const introPlaceholder = `Tell us about yourself. Who are you? What makes you tick? What are your biggest accomplishments and acheivements? What impact do you want to have on your current community, your college and ultimately the world? We know you’re brilliant, be sure to let scholarship providers know how brilliant you are :)`
